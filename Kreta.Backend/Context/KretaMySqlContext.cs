66dac512534ad55794d0e5e4f016cfd4eb28ea6d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-16 13:17:08 +0200

    Generáljuk a konstruktort

--- Kreta.Backend/Context/KretaMySqlContext.cs
+++ Kreta.Backend/Context/KretaMySqlContext.cs
@@ -1,6 +1,11 @@
-﻿namespace Kreta.Backend.Context
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kreta.Backend.Context
 {
     public class KretaMySqlContext : KretaContext
     {
+        public KretaMySqlContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
